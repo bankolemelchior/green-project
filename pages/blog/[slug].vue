835bf3a015294ac4195f74d6f1bd1233c32deddd
bankolemelchior <bankolemelchior@gmail.com>
2025-02-28 19:30:13 +0100

    rendu du contenu de l'aritcle en cours

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -4,9 +4,36 @@ const {data:post} = await useAsyncData('post',() => queryCollection('blog').path
 </script>
 
 <template>
-    <div>
-        <pre>Page: {{post}}</pre>
-    </div>
+        <UContainer v-if="post">
+            <UPageHeader :title="post.title" :description="post.description" >
+                <template #headline>
+                    <UBadge v-bind="post.badge" variant="subtle"/>
+                    <span class="text-gray-700">&middot;</span>
+                    <time :datetime="post.date">{{formatDate(post.date)}}</time>
+                </template>
+
+                <div class="flex flex-wrap items-center gap-2 mt-4">
+                    <UButton
+                    v-for="(auteur, index) in post.auteurs"
+                        :key="index"
+                        color="white"
+                        variant="ghost"
+                        :to="auteur.to"
+                        target="_blank"
+                    >
+                    <UAvatar v-bind="auteur.avatar" :alt="`Avatar de ${auteur.name}`"/>
+                    {{ auteur.name }}
+                    </UButton>
+                </div>
+            </UPageHeader>
+            <!-- Body -->
+             <UPage>
+                 <UPageBody v-if="post">
+                    <ContentRenderer :value="post"/>
+                 </UPageBody>
+             </UPage>
+        </UContainer>
+        
 </template>
 
 <style scoped></style>
